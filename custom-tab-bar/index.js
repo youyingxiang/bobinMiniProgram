474157723c45c5cc715f1a4bf95e241619751e90
TerranChao <wuchaoboy@gmail.com>
2020-07-31 22:07:21 +0800

    modify style

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,35 +1,37 @@
 Component({
-	data: {
-		active: 0,
-		list: [
-			{
-				name: 'home',
-				icon: 'home-o',
-				text: '首页',
-				url: '/pages/home/index'
-			},
-			{
-				name: 'msg',
-				icon: 'user-circle-o',
-				text: '我的',
-				url: '/pages/index/index'
-			}
-		]
-	},
+  data: {
+    active: 0,
+    list: [
+      {
+        name: 'home',
+        icon: 'wap-home-o',
+        text: '首页',
+        url: '/pages/home/index',
+      },
+      {
+        name: 'msg',
+        icon: 'user-circle-o',
+        text: '我的',
+        url: '/pages/index/index',
+      },
+    ],
+  },
 
-	methods: {
-		onChange(event) {
-			this.setData({ active: event.detail });
-			wx.switchTab({
-				url: this.data.list[event.detail].url
-			});
-		},
+  methods: {
+    onChange(event) {
+      this.setData({ active: event.detail });
+      wx.switchTab({
+        url: this.data.list[event.detail].url,
+      });
+    },
 
-		init() {
-			const page = getCurrentPages().pop();
-			this.setData({
-				active: this.data.list.findIndex(item => item.url === `/${page.route}`)
-			});
-		}
-	}
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        active: this.data.list.findIndex(
+          (item) => item.url === `/${page.route}`
+        ),
+      });
+    },
+  },
 });
