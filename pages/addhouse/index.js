cd55e3ec520ad8462fdd02d3d655627b11bcfc6c
TerranChao <wuchaoboy@gmail.com>
2020-08-05 09:38:06 +0800

    update

--- pages/addhouse/index.js
+++ pages/addhouse/index.js
@@ -1,16 +1,20 @@
 import regeneratorRuntime from '../../utils/regenerator-runtime';
 import * as userServices from '../../services/user';
-import Logger from "../../utils/logger";
+import Logger from '../../utils/logger';
+// import cityList from '../../data/area';
 import { redirectTo, showSuccess, showError } from '../../utils/util';
 // pages/addhouse/index.js
 const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    // citySelector: false,
+    // cityList: cityList,
+    // city: null,
     housename: null,
+    number: null,
     address: null,
     phone: null,
     disabled: false,
@@ -19,18 +23,16 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       phone: app.globalData.phone,
-    })
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
   /**
    * 生命周期函数--监听页面显示
    */
@@ -38,42 +40,32 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  },
+  onShareAppMessage: function() {},
   /**
    * @see 提交表单
-   * @param {*} e 
+   * @param {*} e
    */
-  addFormSubmit: async function (e) {
+  addFormSubmit: async function(e) {
     let _this = this;
     let values = e.detail.value;
 
@@ -83,24 +75,44 @@ Page({
     }
     // 按钮禁用
     _this.setData({
-      disabled: true
+      disabled: true,
     });
     const { code, data, message } = await userServices.fetchAddHouse({
+      // // city: values.city,
+      number: values.number,
       housename: values.housename,
       address: values.address,
       phone: values.phone,
-    })
+    });
     if (code == 200) {
-      wx.setStorageSync('addmsg', "添加成功！请等待3-5个工作日")
-      redirectTo('/pages/houselist/index')
+      wx.setStorageSync('addmsg', '添加成功！请等待3-5个工作日');
+      redirectTo('/pages/houselist/index');
     } else {
-      Logger.info("房源添加失败！")
-      showError("房源添加失败！")
+      Logger.info('房源添加失败！');
+      showError('房源添加失败！');
     }
   },
-  validation: function (values) {
+  // showCityPopup: function() {
+  //   this.setData({ citySelector: true });
+  // },
+  // closeCityPopup: function() {
+  //   this.setData({ citySelector: false });
+  // },
+  // changeCityPopup: function(val) {
+  //   console.log(val);
+  //   // this.setData({ city: false });
+  // },
+  validation: function(values) {
+    // if (values.city.length < 1) {
+    //   this.data.error = '请选择城市';
+    //   return false;
+    // }
     if (values.housename.length < 1) {
-      this.data.error = '名称不能为空';
+      this.data.error = '小区名称不能为空';
+      return false;
+    }
+    if (values.number.length < 1) {
+      this.data.error = '请填写房号';
       return false;
     }
     if (values.address.length < 1) {
@@ -114,4 +126,4 @@ Page({
     }
     return true;
   },
-})
+});
--- pages/houselist/index.js
+++ pages/houselist/index.js
@@ -1,13 +1,17 @@
 // pages/houselist/index.js
 import regeneratorRuntime from '../../utils/regenerator-runtime';
 import * as userServices from '../../services/user';
-import Logger from "../../utils/logger";
+import Logger from '../../utils/logger';
 import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
-import { redirectTo, showSuccess, showError, getStorageFlash } from '../../utils/util';
+import {
+  redirectTo,
+  showSuccess,
+  showError,
+  getStorageFlash,
+} from '../../utils/util';
 // pages/addhouse/index.js
 const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -18,107 +22,99 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.getHouseList();    
-    let addmsg = getStorageFlash('addmsg')
+  onLoad: function(options) {
+    this.getHouseList();
+    let addmsg = getStorageFlash('addmsg');
     if (addmsg) {
       Toast.success(addmsg);
     }
   },
   /**
    * @see 预览图片
-   * @param {*} event 
+   * @param {*} event
    */
-  previewImage: function (event) {
+  previewImage: function(event) {
     console.log(event.currentTarget.dataset.img);
     const fileName = event.currentTarget.dataset.img;
-    const suffixIndex = fileName.lastIndexOf(".");
+    const suffixIndex = fileName.lastIndexOf('.');
     const suffix = fileName.substring(suffixIndex + 1).toUpperCase();
-    if (suffix == "BMP" || suffix == "JPG" || suffix == "JPEG" || suffix == "PNG" || suffix == "GIF") {
+    if (
+      suffix == 'BMP' ||
+      suffix == 'JPG' ||
+      suffix == 'JPEG' ||
+      suffix == 'PNG' ||
+      suffix == 'GIF'
+    ) {
       wx.previewImage({
-        urls: [fileName]
-      })
+        urls: [fileName],
+      });
     } else {
       wx.showLoading({
         title: '加载中',
-      })
+      });
       wx.downloadFile({
         // 示例 url，并非真实存在
         url: event.currentTarget.dataset.img,
-        success: function (res) {
-          const filePath = res.tempFilePath
+        success: function(res) {
+          const filePath = res.tempFilePath;
           wx.openDocument({
             filePath: filePath,
-            success: function (res) {
-              console.log('打开文档成功')
+            success: function(res) {
+              console.log('打开文档成功');
             },
             complete: () => {
               wx.hideLoading();
-            }
-          })
-        }
-      })
+            },
+          });
+        },
+      });
     }
   },
-  getHouseList: async function () {
-    const { code, data, message } = await userServices.fetchGetHouseList()
+  getHouseList: async function() {
+    const { code, data, message } = await userServices.fetchGetHouseList();
     if (code == 200) {
       this.setData({
-        houseList: data
+        houseList: data,
       });
     }
   },
 
-  addHouse: function () {
-    redirectTo("/pages/addhouse/index")
+  addHouse: function() {
+    redirectTo('/pages/addhouse/index');
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {},
+});
